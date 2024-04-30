6b7c36c75643b9f205f7c96cd14c1fc006ee748d
aidai <aibekovaaidai123@gmail.com>
2024-04-30 13:22:30 +0000

    main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/Aidai/logo.svg";
+import { Link } from "react-router-dom";
+import Container from "../../helpers/Container";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Container>
+      <div>
+        <div className="flex">
+          <img src={logo} alt="" />
+          <div className="">
+            <ul className="flex">
+              <Link to={"/all"}>
+                <li>ALL PRODUCTS</li>
+              </Link>
+              <Link to={"/about"}>
+                <li>ABOUT SEEDRA</li>
+              </Link>
+              <Link to={"/blog"}>
+                <li>OUR BLOG</li>
+              </Link>
+              <Link to={"/contacts"}>
+                <li>CONTACTS</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
